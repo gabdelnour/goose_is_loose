28628f2f4679f64abc8309c61ed68119cc27173b
Gustavo Abdelnour <gurabdelnour@gmail.com>
2020-07-24 15:50:52 -0400

    added content, and took away unnecessary features (progress bars)

--- src/sections/about/index.js
+++ src/sections/about/index.js
@@ -1,81 +1,119 @@
-import React from 'react'
-import Particles from 'react-particles-js';
-import Progress from 'components/progress'
-import { Row, Col } from 'react-bootstrap'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faLinkedin, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons'
-import ThemeContext from '../../context'
-import './styles.scss'
+import React from "react";
+import Particles from "react-particles-js";
+import Typewriter from "typewriter-effect";
+import { Row, Col } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faLinkedin,
+  faTwitter,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+import ThemeContext from "../../context";
+import "./styles.scss";
 
 class Hero extends React.Component {
+  static contextType = ThemeContext;
 
-    static contextType = ThemeContext
+  render() {
+    return (
+      <section
+        id={`${this.props.id}`}
+        className="about"
+        style={{ height: this.context.height }}
+      >
+        {this.particles()}
+        <Row>
+          <Col md={6} className="content">
+            <div className="content-text">
+              <div className="line-text">
+                <h4>
+                  <Typewriter
+                    options={{
+                      strings: ["Hi there!"],
+                      autoStart: true,
+                      loop: true,
+                    }}
+                  />
+                </h4>
+              </div>
+              <h3>Here is a little about me as a person</h3>
+              <div className="separator" />
+              <p>
+                I was born and raised in Caracas, Venezuela. At 14 I moved to
+                Miami Florida. Upon graduating High School I attended Santa
+                Clara University where I was a D1 rower. By Junior year, I
+                transfered to George Washington where I graduated with a double
+                major on Economics and Political Science.
+                <br></br>
+                <br></br>
+                My passion for software development arrised from my love for
+                data analysis. Being limited by platforms like excel and limited
+                computer power of my laptop. I realized I needed to learn
+                software development, cloud computing, and its most important
+                languages to increase my ability to handle data and create
+                beautiful displays.
+              </p>
+              <div className="social social_icons">
+                <FontAwesomeIcon
+                  icon={faGithub}
+                  className="social_icon"
+                  onClick={() => window.open("https://github.com/gabdelnour")}
+                />
+                {/* <FontAwesomeIcon
+                  icon={faTwitter}
+                  className="social_icon"
+                  onClick={() => window.open("https://www.twitter.com")}
+                />
+                <FontAwesomeIcon
+                  icon={faYoutube}
+                  className="social_icon"
+                  onClick={() => window.open("https://www.youtube.com")}
+                /> */}
+                <FontAwesomeIcon
+                  icon={faLinkedin}
+                  className="social_icon"
+                  onClick={() =>
+                    window.open(
+                      "https://www.linkedin.com/in/gustavo-abdelnour/"
+                    )
+                  }
+                />
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </section>
+    );
+  }
 
-    render() {
-        return (
-            <section id={`${this.props.id}`} className="about" style={{height: this.context.height}}>
-                {this.particles()}
-                <Row>
-                    <Col md={6} className="content">
-                        <div className="content-text">
-                            <div className="line-text">
-                                <h4>About Me</h4>
-                            </div>
-                            <h3>I'm a Full Stack web developer working from home</h3>
-                            <div className="separator" />
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus suscipit nisi vitae feugiat vestibulum. Aliquam porta nulla vel odio scelerisque, pretium volutpat dui euismod. Integer porttitor dolor placerat malesuada dictum. Fusce enim dolor, dignissim quis ornare at, elementum nec turpis. Donec ac interdum libero, sed condimentum lectus. Nunc nec iaculis tortor. Donec interdum sollicitudin eros in pharetra. Donec ultricies laoreet dictum. Maecenas vestibulum sodales justo, id hendrerit orci aliquet gravida. Nulla facilisi.</p>
-                            <div className="social social_icons">
-                                <FontAwesomeIcon icon={faGithub} className="social_icon" onClick={() => window.open('https://www.github.com')}/>
-                                <FontAwesomeIcon icon={faTwitter} className="social_icon" onClick={() => window.open('https://www.twitter.com')} />
-                                <FontAwesomeIcon icon={faYoutube} className="social_icon" onClick={() => window.open('https://www.youtube.com')} />
-                                <FontAwesomeIcon icon={faLinkedin} className="social_icon" onClick={() => window.open('https://www.linkedin.com')} />
-                            </div>
-                        </div>
-                    </Col>
-                    <Col md={6} className="skills">
-                            <div className="line-text">
-                                <h4>My Skills</h4>
-                            </div>
-                            <div className="skills-container">
-                                <Progress name="Web Design" value={90} delay={1100} />
-                                <Progress name="Angular" value={50} delay={1100} />
-                                <Progress name="React" value={80} delay={1100} />
-                                <Progress name="Vue" value={40} delay={1100} />
-                                <Progress name="MongoDB" value={100} delay={1100} />
-                                <Progress name="CSS" value={50} delay={1100} />
-                            </div>
-                    </Col>
-                </Row>
-            </section>
-        )
-    }
-
-    particles() {
-        return (
-            <Particles
-                className="particles"
-                params={{
-                    "particles": {
-                        "number": {
-                            "value": 50,
-                            "density": {
-                                "enable": false,
-                                "value_area": 5000
-                            }
-                        },
-                        "line_linked": {
-                            "enable": true,
-                            "opacity": .5
-                        },
-                        "size": {
-                            "value": 1
-                        },
-                    },
-                    "retina_detect": true
-            }}/>
-        )
-    }
-
+  particles() {
+    return (
+      <Particles
+        className="particles"
+        params={{
+          particles: {
+            number: {
+              value: 50,
+              density: {
+                enable: false,
+                value_area: 5000,
+              },
+            },
+            line_linked: {
+              enable: true,
+              opacity: 0.5,
+              color: "#76D395",
+            },
+            size: {
+              value: 0.5,
+            },
+          },
+          retina_detect: true,
+        }}
+      />
+    );
+  }
 }
 
-export default Hero
+export default Hero;
