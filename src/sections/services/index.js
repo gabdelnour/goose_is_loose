333e9a1467fa91e99589ef8c248e6bf1ac65b892
Gustavo Abdelnour <gurabdelnour@gmail.com>
2020-07-28 00:32:07 -0400

    I'm too lazy to do it now

--- src/sections/services/index.js
+++ src/sections/services/index.js
@@ -197,7 +197,7 @@ class Services extends React.Component {
             line_linked: {
               enable: true,
               opacity: 0.5,
-              color: "#4AE5E5",
+              color: "#D51A35",
             },
             size: {
               value: 0.5,
